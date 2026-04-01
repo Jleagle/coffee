283061d57903ba700a0b63f7c6a1bde4c64cbb08
James Eagle <jimeagle@gmail.com>
2026-04-01 12:52:36 +0100

    Fix timezone

--- main.go
+++ main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	_ "time/tzdata"
 
 	"github.com/Jleagle/coffee/cmd"
 )
 
+var requiredEnvVars = []string{
+	"COFFEE_API_KEY",
+	"COFFEE_AUTH_DOMAIN",
+	"COFFEE_APP_ID",
+	"COFFEE_PROJECT_ID",
+}
+
 func main() {
+
+	os.Setenv("TZ", "Europe/London")
+
+	for _, key := range requiredEnvVars {
+		if os.Getenv(key) == "" {
+			fmt.Fprintf(os.Stderr, "Error: required environment variable %s is not set\n", key)
+			os.Exit(1)
+		}
+	}
+
 	if err := cmd.RootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
