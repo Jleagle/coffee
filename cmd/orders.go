616787c19848749dd07030b06b00212b538e3b7c
James Eagle <jimeagle@gmail.com>
2026-05-07 21:34:52 +0100

    Remove ids

--- cmd/orders.go
+++ cmd/orders.go
@@ -105,11 +105,11 @@ func printOrders(ctx context.Context, cmd *cobra.Command, client *firestore.Clie
 	}
 
 	var buf bytes.Buffer
-	tbl := table.New("#", "Time", "Name", "Drink", "Status", "ID").WithWriter(&buf)
+	tbl := table.New("#", "Time", "Name", "Drink", "Status").WithWriter(&buf)
 
 	for k, o := range orders {
 		ts := time.UnixMilli(o.OrderTimestamp).Format(time.TimeOnly)
-		tbl.AddRow(k+1, ts, o.UserName, o.DrinkName, o.Status, o.ID)
+		tbl.AddRow(k+1, ts, o.UserName, o.DrinkName, o.Status)
 	}
 	tbl.Print()
 
